33da2b4650a0bdd6f1f2be1bbd07112d1ee78486
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2020-07-31 11:22:19 +0200

    D01 ex01

---
+++ D01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ex01.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/07/31 11:15:49 by roybakker     #+#    #+#                 */
+/*   Updated: 2020/07/31 11:20:24 by roybakker     ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
