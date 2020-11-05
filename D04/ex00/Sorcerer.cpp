7e2b075a2186a25e9046dc335d094b923fc70ccf
Roy peter Bakker <rbakker@f1r5s16.codam.nl>
2020-11-05 17:42:45 +0100

    it's a beginning..

--- D04/ex00/Sorcerer.cpp
+++ D04/ex00/Sorcerer.cpp
@@ -6,7 +6,44 @@
 /*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/19 10:58:01 by roybakker     #+#    #+#                 */
-/*   Updated: 2020/08/19 10:58:02 by roybakker     ########   odam.nl         */
+/*   Updated: 2020/11/05 17:42:21 by rbakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Sorcerer.hpp"
+#include <iostream>
+#include <string>
+#include <array>
+
+// Sorcerer::Sorcerer(void)
+// {
+// 	return ;
+// }
+
+Sorcerer::Sorcerer(std::string name, std::string title) : _name(name), _title(title)
+{
+	std::cout << this->_name << ", " << this->_title << ", is born!" << std::endl;
+	return ;
+}
+
+Sorcerer::Sorcerer(Sorcerer const &src)
+{
+	*this = src;
+	return ;
+}
+
+Sorcerer &		Sorcerer::operator=(Sorcerer const &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_title = rhs._title;
+	}
+	return (*this);
+}
+
+Sorcerer::~Sorcerer(void)
+{
+std::cout << this->_name << ", " << this->_title << ", is dead. Consequences will never be the same!" << std::endl;
+	return ;
+}
--- D04/ex00/Sorcerer.hpp
+++ D04/ex00/Sorcerer.hpp
@@ -6,7 +6,29 @@
 /*   By: roybakker <roybakker@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/19 10:58:12 by roybakker     #+#    #+#                 */
-/*   Updated: 2020/08/19 10:58:13 by roybakker     ########   odam.nl         */
+/*   Updated: 2020/11/05 17:42:24 by rbakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SORCERER_CLASS_H
+#define SORCERER_CLASS_H
+#include <string>
+
+class Sorcerer {
+
+private:
+	std::string		_name;
+	std::string		_title;
+
+public:
+
+    virtual Sorcerer(void);
+	Sorcerer(std::string name, std::string title);
+	Sorcerer(Sorcerer const &src);
+    ~Sorcerer(void);
+
+	Sorcerer & operator=(Sorcerer const &rhs);
+
+};
+
+#endif
