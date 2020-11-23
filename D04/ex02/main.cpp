86b6a17421a0a5c7f077f6d30cfc8f9656d5db97
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2020-11-23 11:46:48 +0100

    updated main

--- D04/ex02/main.cpp
+++ D04/ex02/main.cpp
@@ -17,50 +17,49 @@
 #include "TacticalMarine.hpp"
 #include <iostream>
 
-int main()
-{
-    //First we create a simple squad
-    Squad* original_squad = new Squad;
-    ISpaceMarine* bob = new TacticalMarine;
-    ISpaceMarine* jim = new AssaultTerminator;
-
-    original_squad->push(bob);
-    original_squad->push(jim);
-
-    //Second test the copy constructor
-    Squad* copied_squad = new Squad(*original_squad);
-    ISpaceMarine* jerry = new TacticalMarine;
-    ISpaceMarine* jon = new AssaultTerminator;
-
-    copied_squad->push(jerry);
-    copied_squad->push(jon);
-
-    //Third assign the first simple squad to the copied squad
-    *copied_squad = *original_squad;
-
-    //delete the squads and their content
-    delete original_squad;
-    delete copied_squad;
-//    while(1){}
-    return 0;
-}
-
 //int main()
 //{
-//    ISquad* squad = new Squad;
+//    //First we create a simple squad
+//    Squad* original_squad = new Squad;
 //    ISpaceMarine* bob = new TacticalMarine;
 //    ISpaceMarine* jim = new AssaultTerminator;
 //
-//    squad->push(bob);
-//    squad->push(jim);
+//    original_squad->push(bob);
+//    original_squad->push(jim);
 //
-//    for (int i = 0; i < squad->getCount(); ++i)
-//    {
-//        ISpaceMarine* cur = squad->getUnit(i);
-//        cur->battleCry();
-//        cur->rangedAttack();
-//        cur->meleeAttack();
-//    }
-//    delete squad;
+//    //Second test the copy constructor
+//    Squad* copied_squad = new Squad(*original_squad);
+//    ISpaceMarine* jerry = new TacticalMarine;
+//    ISpaceMarine* jon = new AssaultTerminator;
+//
+//    copied_squad->push(jerry);
+//    copied_squad->push(jon);
+//
+//    //Third assign the first simple squad to the copied squad
+//    *copied_squad = *original_squad;
+//
+//    //delete the squads and their content
+//    delete original_squad;
+//    delete copied_squad;
 //    return 0;
 //}
+
+int main()
+{
+    ISquad* squad = new Squad;
+    ISpaceMarine* bob = new TacticalMarine;
+    ISpaceMarine* jim = new AssaultTerminator;
+
+    squad->push(bob);
+    squad->push(jim);
+
+    for (int i = 0; i < squad->getCount(); ++i)
+    {
+        ISpaceMarine* cur = squad->getUnit(i);
+        cur->battleCry();
+        cur->rangedAttack();
+        cur->meleeAttack();
+    }
+    delete squad;
+    return 0;
+}
