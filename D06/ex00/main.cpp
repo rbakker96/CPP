448bd7b7dfd1adc9244576c2c018ab4c9be787ab
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2020-12-01 11:24:44 +0100

    fixed ex00

--- D06/ex00/main.cpp
+++ D06/ex00/main.cpp
@@ -10,11 +10,59 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <exception>
+#include "Convert.hpp"
 #include <iostream>
 
-int main() {
+int     detect_type(std::string literal)
+{
+    size_t i = 0;
+    const char* pseudoliterals[] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
 
+    for (int x = 0; x < 6; x++){
+        if (pseudoliterals[x] == literal)
+            return PSUEDO;
+    }
+    if (literal.length() == 1 && !isdigit(literal[0]))
+        return CHAR;
+    while (literal[i]) {
+        if (literal[i] == '.') {
+            i++;
+            while (isdigit(literal[i])) {
+                i++;
+            }
+            if (literal[i] == 'f' && i == (literal.length() - 1))
+                return FLOAT;
+            else if (!literal[i])
+                return DOUBLE;
+            else
+                return FORMAT_ERROR;
+        }
+        else if (!isdigit(literal[i]) && (i != 0 && (literal[i] == '+' || literal[i] == '-')))
+            return FORMAT_ERROR;
+        i++;
+    }
+    return INT;
+}
 
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        std::cout << "Give one argument" << std::endl;
+    else {
+        int type = detect_type(argv[1]);
+        if (type == -1)
+            std::cout << "Format error" << std::endl;
+        else {
+            Convert convert_tool = Convert(argv[1]);
+            void (Convert::*fptr[6])();
+            fptr[0] = NULL;
+            fptr[1] = &Convert::convert_char;
+            fptr[2] = &Convert::convert_int;
+            fptr[3] = &Convert::convert_float;
+            fptr[4] = &Convert::convert_double;
+            fptr[5] = &Convert::convert_psuedo;
+            (convert_tool.*fptr[type])();
+        }
+    }
     return 0;
 }
