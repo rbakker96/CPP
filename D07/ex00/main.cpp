13badf82d22f91160bb2577dec679f65aa005673
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2020-12-02 18:42:29 +0100

    pointer and refference change

--- D07/ex00/main.cpp
+++ D07/ex00/main.cpp
@@ -13,11 +13,10 @@
 #include <iostream>
 
 template <typename T>
-void    swap(T* first, T* second) {
-    T tmp = *first;
-    *first = *second;
-    *second = tmp;
-
+void    swap(T& first, T& second) {
+    T tmp = first;
+    first = second;
+    second = tmp;
     return ;
 }
 
@@ -31,25 +30,44 @@ T    max(const T& first, const T& second) {
     return((first > second) ? first : second);
 }
 
-int main()
-{
-    int A = 10;
-    int B = 20;
+//int main()
+//{
+//    int A = 10;
+//    int B = 20;
+//
+//    std::cout << "Swap A = " << A << " en B = " << B << std::endl;
+//    swap<int>(A, B);
+//    std::cout << "New A = " << A << " en B = " << B << std::endl;
+//    std::cout << "Min - lowest value = " << min<int>(A, B) << std::endl;
+//    std::cout << "Max - highest value = " << max<int>(A, B) << std::endl;
+//
+//    std::cout << std::endl;
+//
+//    float C = 10.234;
+//    float D = 20.567;
+//
+//    std::cout << "Swap C = " << C << " en D = " << D << std::endl;
+//    swap<float>(C, D);
+//    std::cout << "New C = " << C << " en D = " << D << std::endl;
+//    std::cout << "Min - lowest value = " << min<float>(C, D) << std::endl;
+//    std::cout << "Max - highest value = " << max<float>(C, D) << std::endl;
+//
 
-    std::cout << "Swap A = " << A << " en B = " << B << std::endl;
-    swap<int>(&A, &B);
-    std::cout << "New A = " << A << " en B = " << B << std::endl;
-    std::cout << "Min - lowest value = " << min<int>(A, B) << std::endl;
-    std::cout << "Max - highest value = " << max<int>(A, B) << std::endl;
+int main( void ) {
+    int a = 2;
+    int b = 3;
 
-    std::cout << std::endl;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    float C = 10.234;
-    float D = 20.567;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
-    std::cout << "Swap C = " << C << " en D = " << D << std::endl;
-    swap<float>(&C, &D);
-    std::cout << "New C = " << C << " en D = " << D << std::endl;
-    std::cout << "Min - lowest value = " << min<float>(C, D) << std::endl;
-    std::cout << "Max - highest value = " << max<float>(C, D) << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }
