0aa2bb9df7c7cc6946a0ed202843fddcea856640
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2020-12-04 15:16:50 +0100

    fixed ex00

--- D08/ex00/easyfind.hpp
+++ D08/ex00/easyfind.hpp
@@ -12,8 +12,21 @@
 
 #ifndef EASYFIND_CLASS_H
 #define EASYFIND_CLASS_H
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
 
 template <typename T>
+typename T::iterator     easyfind(T container, const int& target) {
 
+    typename T::iterator iter;
+
+    iter = std::find(container.begin(), container.end(), target);
+    if (iter != container.end())
+        std::cout << "Occurence " << target << " found." << std::endl;
+    else
+        throw std::runtime_error("Occurence not found");
+    return (iter);
+}
 
 #endif
--- D08/ex00/main.cpp
+++ D08/ex00/main.cpp
@@ -10,10 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <vector>
 #include "easyfind.hpp"
 
 int main ()
-(
+{
+    std::vector<int> vect;
 
+    vect.push_back(11);
+    vect.push_back(22);
+    vect.push_back(33);
+    vect.push_back(44);
+    vect.push_back(55);
 
+    try {
+        easyfind(vect, 44);
+        easyfind(vect, 66);
+    }
+    catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
 }
